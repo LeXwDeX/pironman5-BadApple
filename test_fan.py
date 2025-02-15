cf1ec74bb327e394ecd7fadcc6477f70b06e2a9a
lex <sunsan05@gmail.com>
2025-02-15 11:34:07 +0800

    默认rgb为安静模式。

---
+++ test_fan.py
@@ -0,0 +1,13 @@
+from pm_auto.fan_control import FanControl
+
+# 初始化配置
+config = {
+    "gpio_fan_pin": 6,  # 根据实际 GPIO 引脚设置
+    "gpio_fan_mode": 1,  # 模式设置
+}
+
+# 初始化 FanControl
+fan_control = FanControl(config=config, fans=["gpio_fan_state"])
+
+# 手动关闭风扇
+fan_control.off()
